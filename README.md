1e0089bf1f1f9f2bb5a1d1a7c73906e7273f1d4c
abisuresh <aesther2012@gmail.com>
2018-08-17 20:28:37 -0400

    Fixed indenting; Made sure comments were complete; Added logical steps to README.md file

--- README.md
+++ README.md
@@ -6,5 +6,11 @@ Students should use this [rubric](https://review.udacity.com/#!/projects/2696458
 For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
 
 Description of game:
+Goal of game is get player to water side of canvas. The user pushes the left, right, up or down arrow keys to move. There are 3 enemy bugs crawling across the screen and the player has to avoid them while making her/his way across.
 
 General rules:
+- Open webpage
+- Press left, right, up or down arrows to move player
+- Avoid hitting any of the bugs
+- If a player hits a bug (enemy) the game immediately resets and the player is moved back to the start.
+- If the player makes it across safely, a pop-up alert shows up stating the user has won.
--- js/app.js
+++ js/app.js
@@ -1,53 +1,54 @@
 // Enemies our player must avoid
 var Enemy = function(inputSpeed, inputYPosition) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-    this.positionEnemyX = 0;
-    this.speed= 10;
-    this.speed = inputSpeed;
-    this.positionEnemyY = inputYPosition;
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.sprite = 'images/enemy-bug.png';
+  this.positionEnemyX = 0;
+  this.speed= 10;
+  this.speed = inputSpeed;
+  this.positionEnemyY = inputYPosition;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    this.positionEnemyX = this.positionEnemyX + (dt*(this.speed));
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  this.positionEnemyX = this.positionEnemyX + (dt*(this.speed));
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
 
-    //if collision with player
-    if(this.positionEnemyX <= (player.xPos + 50) && this.positionEnemyX >= (player.xPos - 50))
-    {
-      player.reset();
-    }
+  //if collision with player
+  if(this.positionEnemyX <= (player.xPos + 50) && this.positionEnemyX >= (player.xPos - 50) &&
+  this.positionEnemyY <=(player.yPos + 50) && this.positionEnemyY  >= (player.yPos - 50))
+  {
+    player.reset();
+  }
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    this.y = 0;
-    ctx.drawImage(Resources.get(this.sprite), this.positionEnemyX, this.positionEnemyY);
+  this.y = 0;
+  ctx.drawImage(Resources.get(this.sprite), this.positionEnemyX, this.positionEnemyY);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 class Player {
-  //constructor
+  //constructor for player class using 3 parameters
   constructor(xPos, yPos, spriteImage){
 
     //Properties of Player class
+    //Set initial values of x and y to starting tile position
     this.xPos = 200;
     this.yPos = 400;
-    this.spriteImage= 'images/char-boy.png';
+    this.spriteImage= 'images/char-cat-girl.png';
 
   }
-  //methods for Player class
-  //update method
+  //accessor function for returning x and y positions of player
   get pos(){
     return this.xPos, this.yPos;
   }
@@ -61,24 +62,26 @@ class Player {
   //accordingly. Only move player in these directions if the movement would not
   //put the player off the canvas
   handleInput(direction){
-      if('left' == direction && this.xPos>0){
-        this.xPos = this.xPos - 100;
-      }else if('right' == direction && this.xPos<400){
-        this.xPos = this.xPos + 100;
-      }else if('up' == direction && this.yPos>0){
-        this.yPos = this.yPos - 100;
-      }else if('down' == direction && this.yPos<400){
-        this.yPos = this.yPos + 100;
-      }
+    if('left' == direction && this.xPos>0){
+      this.xPos = this.xPos - 100;
+    }else if('right' == direction && this.xPos<400){
+      this.xPos = this.xPos + 100;
+    }else if('up' == direction && this.yPos>0){
+      this.yPos = this.yPos - 100;
+    }else if('down' == direction && this.yPos<400){
+      this.yPos = this.yPos + 100;
+    }
   }
 
+  //reset function that moves player back to original tile
   reset() {
     this.xPos = 200;
     this.yPos = 400;
   }
 
+  //if player reaches water, reset game and alert them they have won
   update(){
-    if(this.yPos<=0)
+    if(this.yPos<= 0)
     {
       player.reset();
       alert('You have won!');
@@ -91,18 +94,18 @@ class Player {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-var allEnemies = [new Enemy(10, 100), new Enemy(20, 200)];
+var allEnemies = [new Enemy(10, 100), new Enemy(20, 200), new Enemy (30, 300)];
 var player= new Player(); //player object
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
